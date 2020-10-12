79a3ea278b21159ece06a63be33244ea78badde1
Edward Clem Williams <ecw@skr-compute1.cs.uchicago.edu>
2020-10-11 20:40:02 -0500

    playing around with gt detection

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 
 export LRU_CACHE_CAPACITY=1
 
-python train.py --config-file singlescale.yml 
+python train.py --config-file train_hotspots.yml 
 
 # python train.py --config-file train_signs.yml 
 
